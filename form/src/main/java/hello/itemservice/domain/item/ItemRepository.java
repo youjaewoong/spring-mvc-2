b50c06f0b520a91c625e793a5abc7b61853020b7
gos1004 <nnv711@gmail.com>
2021-12-26 11:19:29 +0900

    체크박스 - 단일

--- form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -28,10 +28,14 @@ public class ItemRepository {
     }
 
     public void update(Long itemId, Item updateParam) {
-        Item findItem = findById(itemId);
-        findItem.setItemName(updateParam.getItemName());
-        findItem.setPrice(updateParam.getPrice());
-        findItem.setQuantity(updateParam.getQuantity());
+      Item findItem = findById(itemId);
+      findItem.setItemName(updateParam.getItemName());
+      findItem.setPrice(updateParam.getPrice());
+      findItem.setQuantity(updateParam.getQuantity());
+      findItem.setOpen(updateParam.getOpen());
+      findItem.setRegions(updateParam.getRegions());
+      findItem.setItemType(updateParam.getItemType());
+      findItem.setDeliveryCode(updateParam.getDeliveryCode());
     }
 
     public void clearStore() {
