ca558fc050e9c46b6d3f7238dc9273d43f81514d
gos1004 <nnv711@gmail.com>
2021-12-28 23:38:43 +0900

    Bean Validation - 오브젝트 오류

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -46,18 +46,26 @@ public class ValidationItemControllerV3 {
     
     @PostMapping("/add")
     public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-      
-      //검증에 실패하면 다시 입력 폼으로
-      if (bindingResult.hasErrors()) {
-        log.info("errors={} ", bindingResult);
-        return "validation/v3/addForm";
-      }
-      
-      //성공 로직
-      Item savedItem = itemRepository.save(item);
-      redirectAttributes.addAttribute("itemId", savedItem.getId());
-      redirectAttributes.addAttribute("status", true);
-      return "redirect:/validation/v3/items/{itemId}";
+  
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+  
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors={} ", bindingResult);
+            return "validation/v3/addForm";
+        }
+  
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v3/items/{itemId}";
     }
     
     @GetMapping("/{itemId}/edit")
