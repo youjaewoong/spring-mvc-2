7ab045f720d2e544f9b3767c73ad639582b78e7b
gos1004 <nnv711@gmail.com>
2022-01-02 12:10:44 +0900

    서블릿 필터 - 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,5 +1,6 @@
 package hello.login;
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,5 +17,15 @@ public class WebConfig {
     filterRegistrationBean.setOrder(1);
     filterRegistrationBean.addUrlPatterns("/*");
     return filterRegistrationBean;
+  }
+  
+  @Bean
+  public FilterRegistrationBean loginCheckFilter() {
+      FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+      filterRegistrationBean.setFilter(new LoginCheckFilter());
+      filterRegistrationBean.setOrder(2);
+      filterRegistrationBean.addUrlPatterns("/*");
+  
+      return filterRegistrationBean;
   }
 }
