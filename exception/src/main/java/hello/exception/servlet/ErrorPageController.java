a7cd169cca8a379f580052190dd3923b734106be
gos1004 <nnv711@gmail.com>
2022-01-03 23:47:51 +0900

    시작

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,7 +1,13 @@
 package hello.exception.servlet;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
@@ -30,6 +36,21 @@ public class ErrorPageController {
       log.info("errorPage 500");
       printErrorInfo(request);
       return "error-page/500";
+  }
+  
+  @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<Map<String, Object>> errorPage500Api(
+          HttpServletRequest request, HttpServletResponse response) {
+
+      log.info("API errorPage 500");
+
+      Map<String, Object> result = new HashMap<>();
+      Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+      result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+      result.put("message", ex.getMessage());
+
+      Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+      return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
   }
   
   private void printErrorInfo(HttpServletRequest request) {
