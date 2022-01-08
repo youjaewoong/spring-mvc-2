56ac78727120651e99e98742d17170c8f336f831
gos1004 <nnv711@gmail.com>
2022-01-09 00:23:33 +0900

    HandlerExceptionResolver 활용

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import hello.exception.exception.UserException;
 
 @Slf4j
 @RestController
@@ -17,8 +18,13 @@ public class ApiExceptionController {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
+        
         if (id.equals("bad")) {
           throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        
+        if (id.equals("user-ex")) {
+          throw new UserException("사용자 오류");
         }
 
         return new MemberDto(id, "hello " + id);
